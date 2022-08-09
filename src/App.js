64ac648fbb215382fe4bb5f70fd2beceb6a0eb9c
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-09 09:59:38 +0200

    Section5-80-83 Routing - Setting up our Homepage

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
-// Components
-import Directory from './components/categories/directory.component';
+import { Routes, Route } from 'react-router-dom';
 
-// Styles
+import Home from './routes/home/home.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  // With React Router only the component in the element tag with the specific path will be rendered
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
