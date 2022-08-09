0479d15e44526de49006a281ebb01ffdada27322
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-09 11:17:29 +0200

    Section5-87 Styling for Navigation + Logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,17 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
 const Navigation = () => {
   return (
     // This Fragment element wont be rendered in the browser, we wont see it in the DOM tree
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
