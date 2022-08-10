ea33e4275dc2a7089109b37f710597928da4cf58
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-10 09:28:06 +0200

    Section6-92 Authenticating with Firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDaKtRYLeTTukroFNQdeZEdi1uDTmbnH3E',
+  authDomain: 'reactcourse-crwn-clothing-db.firebaseapp.com',
+  projectId: 'reactcourse-crwn-clothing-db',
+  storageBucket: 'reactcourse-crwn-clothing-db.appspot.com',
+  messagingSenderId: '587298134870',
+  appId: '1:587298134870:web:680fe4d2fc66c7c17014f7',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Authentication
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
