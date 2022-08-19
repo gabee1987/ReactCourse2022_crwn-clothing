89596174180e65d9a744c3bd722d477c2f1d297d
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-19 11:41:38 +0200

    Section10-132 Category Preview Component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,7 @@
-import { Fragment, useContext } from 'react';
+import { useContext } from 'react';
 
 import { CategoriesContext } from '../../contexts/categories.context';
-import ProductCard from '../../components/product-card/product-card.component';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
 
 import './shop.styles.scss';
 
@@ -9,20 +9,14 @@ const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   return (
     // The shorthand of the Fragment is just simply <>
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>
-            {title}
-            <div className="products-container">
-              {categoriesMap[title].map((product) => (
-                <ProductCard key={product.id} product={product} />
-              ))}
-            </div>
-          </h2>
-        </Fragment>
-      ))}
-    </Fragment>
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 
