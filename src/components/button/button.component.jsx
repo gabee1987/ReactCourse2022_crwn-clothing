66ecb656a761f5e962e04ef9b7c4011000e86217
Gabor Koncz1 <gabor_koncz1@epam.com>
2022-08-22 14:46:03 +0200

    Section11-137 Styled-Components - Button

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,20 +1,36 @@
-import './button.styles.scss';
+import {
+  BaseButton,
+  GoogleSignInButton,
+  InvertedButton,
+} from './button.styles';
 
 // We will have 3 different styles of buttons in the application, so we need to create 3 different classname for this button component
-const BUTTON_TYPES_CLASSES = {
+export const BUTTON_TYPES_CLASSES = {
+  base: 'base',
   google: 'google-sign-in',
   inverted: 'inverted',
 };
 
+const getButton = (buttonType = BUTTON_TYPES_CLASSES.base) =>
+  ({
+    [BUTTON_TYPES_CLASSES.base]: BaseButton,
+    [BUTTON_TYPES_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPES_CLASSES.inverted]: InvertedButton,
+  }[buttonType]);
+
 const Button = ({ children, buttonType, ...otherProps }) => {
-  return (
-    <button
-      className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  );
+  const CustomButton = getButton(buttonType);
+  return <CustomButton {...otherProps}>{children}</CustomButton>;
+
+  // Old button format for sass styling
+  // return (
+  //   <button
+  //     className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`}
+  //     {...otherProps}
+  //   >
+  //     {children}
+  //   </button>
+  // );
 };
 
 export default Button;
